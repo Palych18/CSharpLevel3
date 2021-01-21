8fb02b7ce8f7f4a9f7fa6d1cddb5ef722bd50ef8
Palych18 <pavel_egoroff@mail.ru>
2021-01-21 16:39:39 +0300

    Домашнее задание к первому уроку.

---
+++ HomeWork1/HomeWork1/MyButton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace HomeWork1
+{
+    class MyButton : ButtonBase
+    {
+        public static readonly DependencyProperty HomeWork1Property;
+
+        static MyButton()
+        {
+
+            MyButton.HomeWork1Property = DependencyProperty.Register("HomeWork1",
+                typeof(bool), //тип
+                typeof(MyButton), //класс
+                new FrameworkPropertyMetadata(false, new PropertyChangedCallback(HomeWork1PropertyChanged)));
+        }
+
+        private static void HomeWork1PropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+           
+        }
+
+        public bool HomeWork1
+        {
+            get { return (bool)GetValue(MyButton.HomeWork1Property); }
+            set { SetValue(MyButton.HomeWork1Property, value); }
+        }
+    }
+}
