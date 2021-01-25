d7899eb0bd9f709c1337f25a1b38f9676c76b28a
Palych18 <pavel_egoroff@mail.ru>
2021-01-25 21:00:05 +0300

    Домашнее задание к третьему уроку.

---
+++ HomeWork3/MyNotepad/MyNotepad/Model/Account.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotepad.Model
+{
+    public class Account
+    {
+        public string Login { get; set; } = "None";
+        public string Password { get; set; } = "None";
+        public Account(string login, string password)
+        {
+            Login = login;
+            Password = password;
+        }
+    }
+
+    class Accounts
+    {
+        public bool Checks(Account account)
+        {
+            foreach (Account acc in ListAccounts)
+                if (acc.Login == account.Login && acc.Password == account.Password) return true;
+            return false;
+        }
+        
+         public static IEnumerable<Account> ListAccounts
+        {
+            get;
+        }
+            = new List<Account>() {
+                                  new Account("root", "root"),
+                                  new Account("login","password"),
+                                  new Account("admin","admin")
+                                  };
+    }
+}
---
+++ HomeWork3/MyNotepad/MyNotepad/ViewModel/ViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MyNotepad.ViewModel
+{
+    public class ViewModel : INotifyPropertyChanged
+    {                
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        bool access;
+        int attempCount = 0;
+
+        public int AttemptCount
+        {
+            get
+            {
+                return attempCount;
+            }
+            set
+            {
+                attempCount = value;
+                PropertyChanged?.Invoke(this,new PropertyChangedEventArgs("AttemptCount"));
+            }
+        }
+
+        public Model.Account Account { get; set; } = new Model.Account("", "");
+        
+        Model.Accounts accounts = new Model.Accounts();
+
+        private void Execute(object obj)
+        {
+            Access = accounts.Checks(Account);
+            AttemptCount++;
+        }
+
+        public ICommand ClickAccess
+        {
+            get
+            {
+                return new DelegateCommand(Execute,CanExecute);
+            }
+        }
+
+        private bool CanExecute(object obj)
+        {
+            return AttemptCount < 3 && Access==false;
+        }
+
+        public bool Access
+        {
+            get
+            {
+                return access;
+            }
+            set
+            {
+                if (access != value)
+                {
+                    access = value;
+                    Console.WriteLine(access);
+                    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Access"));
+                }
+            }
+        }
+    }
+}
